29c2228655c0cc603d118139d5a7839bf0d6dbdf
Daniel Silva <danielfaleirosilva@gmail.com>
2019-04-25 19:09:48 -0300

    Corrige disposição dos elementos do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,38 +6,40 @@ import {IoIosTelephone, IoEmail, IoLocation, IoSocialFacebook, IoSocialInstagram
 const Footer = () => {
     const iconSize = 40;
 
-    const goToFacebook = () => {
-
-    };
-
     return (
-        <div className='bg-ecma-blue pt4 pb4'>
-            <div className='min-max-width center flex-row flex-wrap-reverse'>
-                <img  className='footer-logo ml5' src={logo} alt='Logo' />
-                <div className='white roboto center flex-column footer-wrapper'>
-                    <div className='start flex-row items-center'>
-                        <IoLocation className='footer-icon' size={iconSize}/>
-                        <p className='footer-text'>Rua 85A - Qd. F-16, Lote 13 St. Sul<br/>Goiânia - GO<br/>CEP: 74080-020</p>
-                    </div>
+        <div className='center bg-ecma-blue pt4 pb4 white roboto'>
+            <div className='min-max-width hma flex justify-between footer-inner'>
+                <div className='center flex-column'>
+                    <img  className='footer-logo' src={logo} alt='Logo' />
                 </div>
-                <div className='white roboto center flex-column footer-wrapper'>
+                <div className='center flex-column footer-info'>
+                    <div className='start flex-row items-center footer-info-left'>
+                        <IoLocation className='footer-icon' size={iconSize}/>
+                        <p className='footer-text'>
+                            Rua 85A - Qd. F-16, Lote 13 St. Sul<br/>
+                            Goiânia - GO<br/>
+                            CEP: 74080-020
+                        </p>
+                    </div>
                     <div className='start flex-row items-center'>
                         <IoIosTelephone className='footer-icon' size={iconSize}/>
-                        <p className='footer-text'>(62) 9 9105-0615</p>
+                        <p className='footer-text'>
+                            (62) 3926-1118 / (62) 9 9105-0615
+                        </p>
                     </div>
                     <a className='start items-center flex-row footer-link no-underline' href='mailto:raydelmm@gmail.com'>
-                        <IoEmail className='footer-icon self-start' size={iconSize}/>
+                        <IoEmail className='footer-icon' size={iconSize}/>
                         <p className='footer-text'>
                             raydelmm@gmail.com
                         </p>
                     </a>
                 </div>
-                <div className='white center flex-column-l flex-row-m flex-row-ns items-center footer-wrapper'>
+                <div className='center footer-social-media items-center'>
                     <a className='footer-link' href='https://www.facebook.com/espacoraydelmarques/'>
-                        <IoSocialFacebook onClick={goToFacebook} size={iconSize*1.25}/>
+                        <IoSocialFacebook className='social-icon' size={iconSize*1.5}/>
                     </a>
                     <a className='footer-link' href='https://www.instagram.com/espacoecma/'>
-                        <IoSocialInstagram size={iconSize*1.25}/>
+                        <IoSocialInstagram className='social-icon' size={iconSize*1.5}/>
                     </a>
                 </div>
             </div>
