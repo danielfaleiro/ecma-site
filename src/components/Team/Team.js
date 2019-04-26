256f1599357eec2f82e2b8acfbc63e296e82bf91
Daniel Silva <danielfaleirosilva@gmail.com>
2019-04-26 18:46:40 -0300

    Novo Design da página Equipe (Team)

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,25 +4,37 @@ import TeamCard from './TeamCard';
 
 const Team = () => {
     return (
-        <div className='min-max-width top-page section-container center flex-column'>
-            <p className='title color-ecma-gold tc lato'>Nossa Equipe</p>
-            <div className='hma flex justify-around flex-wrap'>
+        <div className='container-vpa min-max-width top-page section-container center flex-column'>
+            <div className='title color-ecma-gold tc lato'>Nossa Equipe</div>
+            <div className='hma flex justify-between flex-column flex-wrap'>
                 <TeamCard
-                    name='Raydel Marques'
+                    index={1}
+                    name='RAYDEL MARQUES'
                     job='Fisioterapeuta'
                     credentials='CREFITO: 11/XXXXX-X'
                     phone='(62) XXXX-XXXX'
                     email='fisioterapia@espacoecma.com.br'
                 />
                 <TeamCard
-                    name='Luana Faleiro'
+                    index={2}
+                    name='LUANA FALEIRO'
                     job='Educadora Física'
                     credentials='CREF XXXXXXX'
                     phone='(62) XXXX-XXXX'
                     email='treinamento@espacoecma.com.br'
+                    flip={true}
                 />
                 <TeamCard
-                    name='Nome'
+                    index={3}
+                    name='NOME SOBRENOME'
+                    job='Instrutora de Pilates'
+                    credentials='Credencial'
+                    phone='(62) XXXX-XXXX'
+                    email='pilates@espacoecma.com.br'
+                />
+                <TeamCard
+                    index={4}
+                    name='NOME SOBRENOME'
                     job='Instrutora de Pilates'
                     credentials='Credencial'
                     phone='(62) XXXX-XXXX'
--- src/components/Team/TeamCard.js
+++ src/components/Team/TeamCard.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TeamCard.css';
 
-const TeamCard = ({name, job, credentials, phone, email}) => {
-    return (
-        <div className='member-container roboto'>
-            <div className='member-photo'></div>
-            <div className='mh3'>
-                <h3>{name}</h3>
-                <p>{job}</p>
-                <p>{credentials}</p>
-                <p className='mt4'><strong>Telefone:</strong></p>
-                <p>{phone}</p>
-                <p><strong>E-mail:</strong></p>
-                <p><a href={'mailto:' + email}>{email}</a></p>
+class TeamCard extends Component {
+    componentDidMount() {
+        const index = this.props.index;
+        const memberCard = document.getElementById(`member${index}`);
+        const memberInfo = document.getElementById(`memberInfo${index}`);
+
+        if (index % 2 === 0) {
+            memberCard.classList.add("flex-row-reverse");
+            memberInfo.classList.add("flex-row-reverse");
+        } else {
+            memberCard.classList.add("flex-row");
+            memberInfo.classList.add("flex-row");
+        }
+    }
+
+    render() {
+        const { index, name, job, credentials, phone, email } = this.props;
+        return (
+            <div id={`member${index}`} className='min-max-width start flex-wrap member-container lato'>
+                <div className='member-photo center'>Foto</div>
+                <div id={`memberInfo${index}`} className='member-info center flex-wrap'>
+                    <div className='member-info-1'>
+                        <h2>{name}</h2>
+                        <p className='member-text'>
+                            {job}
+                        </p>
+                        <p>
+                            {credentials}
+                        </p>
+                    </div>
+                    <div className='member-info-2 ts start flex-column'>
+                        <span>
+                            <p><strong>Telefone:</strong></p>
+                            <p>{phone}</p>
+                        </span>
+                        <span>
+                            <p><strong>E-mail:</strong></p>
+                            <p><a href={'mailto:' + email}>{email}</a></p>
+                        </span>
+                    </div>
+                    <div className='member-info-3'>
+                        Texto
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default TeamCard;
