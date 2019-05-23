e3a578c7251c3b41f0b85b45d5d48a11b91da7c9
Daniel Silva <danielfaleirosilva@gmail.com>
2019-05-22 23:25:07 -0300

    Base do carrossel pronta e responsiva

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,7 +4,6 @@ import './Carousel.css';
 import links from '../../links';
 import Slider from "react-slick";
 
-
 const Carousel = () => {
     var settings = {
         dots: true,
@@ -12,23 +11,41 @@ const Carousel = () => {
         swipeToSlide: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         speed: 1000,
         autoplaySpeed: 5000,
         cssEase: "linear",
+        appendDots: dots => (
+          <div
+            style={{
+              position: "relative",
+              top: "-45px",
+              padding: "10px",
+            }}
+          >
+            <ul 
+              style={{
+                margin: "0px",
+                paddingInlineStart: "0",
+              }}
+            > {dots} </ul>
+          </div>
+        ),
       };
 
     return (
         <div id='carousel' className='carousel top-page'>
             <Slider {...settings}>
                 <div className='carousel-container'>
-                    <h1 className='carousel-title mb0 hpa white roboto'>
-                        Fisioterapia, Pilates e Personal Trainer em um único lugar
-                    </h1>
-                    <p className='carousel-subtitle hpa white lato'>
-                        Temos a solução completa para sua saúde e bem-estar.
-                    </p>
-                    <FirstButton destination={links.services.self} text={'Conheça Nossas Especialidades'}/>
+                    <div className='min-max-width center flex-column'>
+                        <h1 className='carousel-title mb0 hpa white roboto'>
+                            Fisioterapia, Pilates e Personal Trainer em um único lugar
+                        </h1>
+                        <p className='carousel-subtitle hpa white lato'>
+                            Temos a solução completa para sua saúde e bem-estar.
+                        </p>
+                        <FirstButton destination={links.services.self} text={'Conheça Nossas Especialidades'}/>
+                    </div>
                 </div>
             </Slider>
             
