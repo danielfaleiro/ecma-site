adbc716f3fbdefab2d92a315a348d930e19e1a46
Daniel Silva <danielfaleirosilva@gmail.com>
2019-11-23 17:54:42 -0300

    Atualiza os depoimentos de clientes.

--- src/components/Testimonials/data.js
+++ src/components/Testimonials/data.js
@@ -4,12 +4,12 @@ const data = [
         "testmony": "Pratico tênis com frequência e contusoẽs, dores e desgastes são comuns para mim. Mas sempre conto com a ECMA para ficar 100% novamente e continuar praticando!"
     },
     {
-        "name": "JOAQUINA PEREREIRA",
-        "testmony": "Minha vida melhorou muito depois que passei a praticar pilatates no ECMA."
+        "name": "EDUARDO DE ALMEIA XAVIER",
+        "testmony": "Excelente lugar para a prática de exercícios físicos, com orientação de profissionais capacitados. Um espaço que engloba tanto a parte de fisioterapia e reabilitação fisiopatológica, como a parte de fortalecimento muscular e articular."
     },
     {
-        "name": "MARIA DE LURDES",
-        "testmony": "Quando fazia academia, meu treino não rendia muito e me faltava motivação. Mas com a professora Luana na ECMA, finalmente alcancei meu potencial de treino!"
+        "name": "VALÉRIA TOLEDO GOMIDE",
+        "testmony": "Agradeço imensamente por tudo que a clínica fez por mim! Profissionais muito dedicados e extremamente competentes! Nota 10 para o Pilates especialmente!"
     }
 ];
 
