7e2f7b26bf4ab9955dad530c1920911fdea4e1f6
Daniel Silva <danielfaleirosilva@gmail.com>
2020-01-04 17:38:20 -0300

    Atualiza links para lançamento do site.

--- src/links.js
+++ src/links.js
@@ -1,17 +1,17 @@
 const links = {
-    home: '/ecma-site',
-    clinic: '/ecma-site/espaco',
-    faith: '/ecma-site/nossaconfessionalidade',
-    team: '/ecma-site/equipe',
+    home: '/',
+    clinic: '/espaco',
+    faith: '/nossaconfessionalidade',
+    team: '/equipe',
     services: {
-        self: '/ecma-site/servicos',
-        fisio: '/ecma-site/servicos/fisioterapia',
-        osteo: '/ecma-site/servicos/osteopatia',
-        pilates: '/ecma-site/servicos/pilates',
-        training: '/ecma-site/servicos/personal',
-        relax: '/ecma-site/servicos/espacorelaxar',
+        self: '/servicos',
+        fisio: '/servicos/fisioterapia',
+        osteo: '/servicos/osteopatia',
+        pilates: '/servicos/pilates',
+        training: '/servicos/personal',
+        relax: '/servicos/espacorelaxar',
     },
-    contact: '/ecma-site/contato',
+    contact: '/contato',
     whatsapp: 'https://wa.me/5562991050615?text=Olá.%20Gostaria%20de%20saber%20mais%20sobre%20os%20serviços%20do%20ECMA.',
 }
 
