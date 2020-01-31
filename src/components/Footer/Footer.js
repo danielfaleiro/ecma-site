64bad4a059ada05077dcd8e6c2ac6adef7ac95a9
Daniel Silva <danielfaleirosilva@gmail.com>
2020-01-31 00:51:10 -0300

    Melhorias no Footer
    - Adiciona label para cada informação de contato
    - Corrige margens no modo responsivo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,43 +6,35 @@ import {IoSocialWhatsappOutline, IoIosTelephone, IoEmail, IoLocation, IoSocialFa
 
 const Footer = () => {
     const iconSize = 40;
-
     return (
-        <div className='footer center bg-ecma-blue pt4 pb4 white roboto'>
-            <div className='min-max-width flex justify-between footer-inner'>
+        <div className='footer center bg-ecma-blue pt4 pb4 white roboto footer-text'>
+            <div className='min-max-width hm-default flex justify-between footer-inner'>
                 <div className='center items-center flex-column footer-logo-container'>
-                    <img src={logo} alt='Logo' />
+                    <img src={logo} alt='Logo do ECMA'/>
                 </div>
                 <div className='center items-center flex-column footer-info'>
-                    <a className='start items-center flex-row footer-link' href={links.whatsapp}>
-                        <IoSocialWhatsappOutline className='footer-icon' size={iconSize}/>
-                        <p className='footer-text'>
-                            (62) 9 9105-0615
-                        </p>
-                    </a>
-                    <div className='start flex-row items-center'>
-                        <IoIosTelephone className='footer-icon' size={iconSize}/>
-                        <p className='footer-text'>
-                            <a className='footer-link' href='tel:06239261118'>(62) 3926-1118</a>
-                            <span> / </span>
-                            <a  className='footer-link' href='tel:062991050615'>(62) 9 9105-0615</a>
-                        </p>
-                    </div>
-                    
-                    <a className='start items-center flex-row footer-link' href='mailto:contato@espacoecma.com.br'>
-                        <IoEmail className='footer-icon' size={iconSize}/>
-                        <p className='footer-text'>
-                            contato@espacoecma.com.br
-                        </p>
-                    </a>
-                    <div className='start flex-row items-center footer-info-left'>
-                        <IoLocation className='footer-icon' size={iconSize}/>
-                        <p className='footer-text'>
-                            Rua 85A - Qd. F-16, Lote 13 St. Sul<br/>
-                            Goiânia - GO<br/>
-                            CEP: 74080-020
-                        </p>
-                    </div>
+                    <div className='contact-info ma2 tl'>
+                      <IoSocialWhatsappOutline size='25'/>
+                      <div><strong>WhatsApp</strong></div>
+                      <span></span>
+                      <a href={links.whatsapp} className='grid-gap footer-link'>(62) 9 9105-0615</a>
+                      <IoIosTelephone size='25'/>
+                      <div><strong>Telefone</strong></div>
+                      <span></span>
+                      <div className='grid-gap'>
+                          <a href='tel:06239261118' className='footer-link'>(62) 3926-1118</a>
+                          <span> / </span>
+                          <a href='tel:062991050615' className='footer-link'>(62) 9 9105-0615</a>
+                      </div>
+                      <IoEmail size='25'/>
+                      <div><strong>E-mail</strong></div>
+                      <span></span>
+                      <a href='mailto:contato@espacoecma.com.br' className='grid-gap footer-link'>contato@espacoecma.com.br</a>
+                      <IoLocation size='25' />
+                      <div><strong>Endereço</strong></div>
+                      <span></span>
+                      <div className='grid-gap'>Rua 85A Qd. F-16, Lote 13 St. Sul<br />Goiânia - GO<br />CEP: 74080-020</div>
+                  </div>
                 </div>
                 <div className='center footer-social-media items-center'>
                     <a className='footer-link' href='https://www.facebook.com/espacoraydelmarques/'>
