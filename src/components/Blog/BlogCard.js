6ef3f786497fd05061af4e618c76d64b0bc863c4
Daniel <danielfaleirosilva@gmail.com>
2020-08-10 10:58:20 -0300

    Mudanças para BlogCard

--- src/components/Blog/BlogCard.js
+++ src/components/Blog/BlogCard.js
@@ -5,13 +5,11 @@ import AnchorButton from '../AnchorButton/AnchorButton'
 const BlogCard = ({ title, date, description, image, link, author }) => {
   return (
     <div className='blog-card'>
+      <h3 className='blog-card-title'>{title}</h3>
+      <span className='blog-card-date'> {author} &nbsp;&bull; {date}</span>
       <img src={image} className="blog-card-image" alt='Arte da capa do artigo' />
-      <div className='blog-card-content'>
-        <h3 className='blog-card-title'>{title}</h3>
-        <span className='blog-card-date'> {author} &nbsp;&bull; {date}</span>
-        <p>{description}</p>
-        <AnchorButton text='Leia mais' to={link} />
-      </div>
+      <p>{description}</p>
+      <AnchorButton text='Leia mais' to={link} />
     </div>
   )
 }
