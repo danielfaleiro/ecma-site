4ef0075a91ac4cda40bca6505ab297669ce401c3
Daniel <danielfaleirosilva@gmail.com>
2020-08-17 11:14:18 -0300

    Correções no BlogMiniCard
    - link funcionando
    - cor de hover agora é azul padrão

--- src/components/Blog/BlogMiniCard.js
+++ src/components/Blog/BlogMiniCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './BlogMiniCard.css'
+import Anchor from '../Anchor/Anchor'
 
-const BlogMiniCard = ({ url, title, img, description }) => {
+const BlogMiniCard = ({ link, title, img, description }) => {
   return (
-    <a href={url} className='blog-mini-card lato'>
+    <Anchor to={link} className='blog-mini-card lato'>
       <img src={img} alt='Arte da capa do artigo' />
       <p><strong>{title}</strong></p>
       <p>{description}</p>
-    </a>
+    </Anchor>
   )
 }
 
